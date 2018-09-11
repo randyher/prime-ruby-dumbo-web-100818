98dff30d06f8ac4603783ff6bb1cf105a692a656
Randy Herasme <randyherasme6@gmail.com>
2018-09-11 16:13:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  if (num<=1)
+  return false
+  end
+  arr=[]
+  i=2
+  while i<num
+  arr.push(i)
+  i+=1
+  end
+  y=0
+  while y<arr.length
+  if (num % arr[y]==0)
+  return false
+  end
+  y+=1
+  end
+  return true
+end
