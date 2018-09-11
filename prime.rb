d32199a7ff497fd7c0b57465e2d80ef3366ff31c
Randy Herasme <randyherasme6@gmail.com>
2018-09-11 16:11:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,19 @@
- def prime? (num)
-  for d in 2..(num - 1)
-   if (num % d) == 0
-    return false
-   end
-   end
-   return true
+def prime?(num)
+  if (num<0)
+  return false
   end
+  arr=[]
+  i=2
+  while i<num
+  arr.push(i)
+  i+=1
+  end
+  y=0
+  while y<arr.length
+  if (num % arr[y]==0)
+  return false
+  end
+  y+=1
+  end
+  return true
+end
